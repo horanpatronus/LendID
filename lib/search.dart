e95655940536d8caeff9afdfb5faa64f5175338f
giovanialvin <alvingiovani17@upi.edu>
2023-05-20 18:26:21 +0700

    aa

--- lib/search.dart
+++ lib/search.dart
@@ -26,12 +26,6 @@ List<Product> products = [
   Product(name: 'Product 6', description: 'Description of Product 6'),
   Product(name: 'Product 6', description: 'Description of Product 6'),
   Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
-  Product(name: 'Product 6', description: 'Description of Product 6'),
 ];
 
 class SearchButton extends StatelessWidget {
