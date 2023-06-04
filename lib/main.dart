62e996ddc112e36fc19d462d8b0feb2c85542b89
Mdhstama <mdhstama.utama@gmail.com>
2023-06-04 20:52:28 +0700

    update topup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:homepage/views/edit_profile_perusahaan.dart';
-import 'package:homepage/views/profile_user.dart';
-import 'package:homepage/views/topup.dart';
-import 'package:homepage/views/topup_status.dart';
-import 'package:homepage/views/topup_withdraw.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 
 import 'views/intro.dart';
-import 'views/landing_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -28,7 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Introduction screen',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: WithdrawPage(),
+      home: OnBoardingPage(),
     );
   }
 }
