3fa4d77b3122cd427d6610f09802982f997a84d7
Hafizh Lutfi Hidayat <hafizh.lutfihidayat@gmail.com>
2023-06-18 11:03:03 +0700

    investasi investor backend

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       title: 'Introduction screen',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+      home: LandingPage(),
     );
   }
 }
